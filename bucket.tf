fc4db33943bc4a8571babdbbb802950b631ca38a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-29 13:39:21 -0500

    Rename resource and change encryption for bucket

--- bucket.tf
+++ bucket.tf
@@ -2,13 +2,12 @@
 # Create the S3 bucket where the Terraform state will be stored.
 # ------------------------------------------------------------------------------
 
-resource "aws_s3_bucket" "state_bucket" {
-  acl    = "private"
+resource "aws_s3_bucket" "the_bucket" {
   bucket = var.bucket_name
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
