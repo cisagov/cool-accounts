ecc01dcc8ccad6ff9a1079040c04c2228686ef10
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-29 14:50:30 -0500

    Add role ARN to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,11 @@ output "bucket_arn" {
   description = "The ARN of the S3 bucket where Terraform state information will be stored."
 }
 
+output "role_arn" {
+  value       = aws_iam_role.the_role.arn
+  description = "The ARN of the IAM role that allows sufficient access to the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+}
+
 output "table_id" {
   value       = aws_dynamodb_table.the_table.id
   description = "The ID of the DynamoDB table that to be used for Terraform state locking."
