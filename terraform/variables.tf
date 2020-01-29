fa924bb4cabfc06f4ee2c006a3be82b0e34ee449
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-29 14:38:23 -0500

    Add code to create TF access role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,11 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
+variable "trusted_account_ids" {
+  type        = list(string)
+  description = "A list of IDs of accounts that are allowed to assume the role that allows sufficient access to the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+}
+
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
