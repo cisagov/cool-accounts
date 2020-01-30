06c4a093e3eb7516952808b8fa59fbb00205921b
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-29 22:15:39 -0500

    Edit outputs to agree with new resource names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,24 @@
-output "bucket_id" {
-  value       = aws_s3_bucket.the_bucket.id
+output "state_bucket_id" {
+  value       = aws_s3_bucket.state_bucket.id
   description = "The ID of the S3 bucket where Terraform state information will be stored."
 }
 
-output "bucket_arn" {
-  value       = aws_s3_bucket.the_bucket.arn
+output "state_bucket_arn" {
+  value       = aws_s3_bucket.state_bucket.arn
   description = "The ARN of the S3 bucket where Terraform state information will be stored."
 }
 
-output "role_arn" {
-  value       = aws_iam_role.the_role.arn
+output "backend_role_arn" {
+  value       = aws_iam_role.backend_role.arn
   description = "The ARN of the IAM role that allows sufficient access to the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
 }
 
-output "table_id" {
-  value       = aws_dynamodb_table.the_table.id
+output "state_lock_table_id" {
+  value       = aws_dynamodb_table.state_lock_table.id
   description = "The ID of the DynamoDB table that to be used for Terraform state locking."
 }
 
-output "table_arn" {
-  value       = aws_dynamodb_table.the_table.arn
+output "state_lock_table_arn" {
+  value       = aws_dynamodb_table.state_lock_table.arn
   description = "The ARN of the DynamoDB table that to be used for Terraform state locking."
 }
