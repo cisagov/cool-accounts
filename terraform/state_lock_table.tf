78dd5f20f86f2d80f6e8a70bd25619c932b69b74
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-29 22:26:44 -0500

    Add missing-but-required hash_key attribute

--- terraform/state_lock_table.tf
+++ terraform/state_lock_table.tf
@@ -4,6 +4,11 @@
 # ------------------------------------------------------------------------------
 
 resource "aws_dynamodb_table" "state_lock_table" {
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  hash_key       = "LockID"
   name           = var.table_name
   read_capacity  = var.table_read_capacity
   write_capacity = var.table_write_capacity
