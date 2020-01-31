2fdf964b957f2cacc3aceebd7328256b53df9b6b
Dave Redmin <david.redmin@beta.dhs.gov>
2020-01-31 09:45:07 -0500

    Comment out assume_role in AWS provider and add local profile name, along with comments explaining what to comment/uncomment before/after bootstrapping

--- users/provider.tf
+++ users/provider.tf
@@ -1,6 +1,12 @@
 provider "aws" {
   region = var.aws_region
-  assume_role {
-    role_arn = var.access_terraform_backend_role_arn
-  }
+  # Use this role once the account has been bootstrapped.
+  # assume_role {
+  #   role_arn = var.access_terraform_backend_role_arn
+  # }
+  # Use the profile below, defined using programmatic credentials for
+  # AWSAdministratorAccess as obtained for the COOL "users" account
+  # from the AWS SSO page, to bootstrap the account.
+  # Comment out the profile below after this account has been bootstrapped.
+  profile = "cool-users-account-admin"
 }
