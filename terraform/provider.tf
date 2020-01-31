b49980d60f7091ad6f32a0502d956021dece7488
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-31 08:38:57 -0500

    Stop using var.create_role_arn
    Add a profile section for bootstrapping, and add comments explaining
    when to use each section.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = var.aws_region
-  assume_role {
-    role_arn = var.create_role_arn
-  }
+  # Use this role once the account has been bootstrapped.
+  # assume_role {
+  #   role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.create_role_name}",
+  # }
+  # Use this profile, defined using programmatic credentials for
+  # AWSAdministratorAccess as obtained for the COOL terraform account
+  # from the AWS SSO page, to bootstrap the account.
+  profile = "cool-terraform-account-admin"
 }
