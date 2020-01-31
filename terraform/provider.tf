f804798043acead33e24196101a1a0fe05ba56ad
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-31 14:36:44 -0500

    Remove extraneous comma
    This little comma breaks the provider configuration.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
   # Use this role once the account has been bootstrapped.
   # assume_role {
-  #   role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.create_role_name}",
+  #   role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.create_role_name}"
   # }
   # Use this profile, defined using programmatic credentials for
   # AWSAdministratorAccess as obtained for the COOL terraform account
