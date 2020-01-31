e83e4c284ae812e6566f974072ad5344352795d3
Dave Redmin <david.redmin@beta.dhs.gov>
2020-01-31 09:41:16 -0500

    Comment out backend config and add comment explaining when it should be commented vs. uncommented

--- users/backend.tf
+++ users/backend.tf
@@ -1,10 +1,12 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
-    dynamodb_table = "terraform-state-lock"
-    profile        = "cool-terraform-backend"
-    region         = "us-east-1"
-    key            = "cool-accounts/users.tfstate"
-  }
-}
+# Comment out the contents of this file when bootstrapping this
+# account.
+# terraform {
+#   backend "s3" {
+#     encrypt        = true
+#     bucket         = "cisa-cool-terraform-state"
+#     dynamodb_table = "terraform-state-lock"
+#     profile        = "cool-terraform-backend"
+#     region         = "us-east-1"
+#     key            = "cool-accounts/users.tfstate"
+#   }
+# }
