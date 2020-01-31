090ffe413691643668df9ee2e5b9203eae3cfbbf
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-31 15:02:30 -0500

    Use the IAM admin role in the provider
    Now that the IAM admin role has been created, we no longer need to use
    the SSO Admin programmatic creds.

--- users/provider.tf
+++ users/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = var.aws_region
   # Use this role once the account has been bootstrapped.
-  # assume_role {
-  #   role_arn = var.access_terraform_backend_role_arn
-  # }
+  assume_role {
+    role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.iam_admin_role_name}"
+  }
   # Use the profile below, defined using programmatic credentials for
   # AWSAdministratorAccess as obtained for the COOL "users" account
   # from the AWS SSO page, to bootstrap the account.
   # Comment out the profile below after this account has been bootstrapped.
-  profile = "cool-users-account-admin"
+  # profile = "cool-users-account-admin"
 }
