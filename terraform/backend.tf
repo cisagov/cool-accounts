e018cd665ec32daab6bb9b5b6172c6966c54310a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-31 08:43:02 -0500

    Add comment explaining that the backend config should be commented out when bootstrapping the account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,12 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "cisa-cool-terraform-state"
-    dynamodb_table = "terraform-state-lock"
-    profile        = "terraform-role"
-    region         = "us-east-1"
-    key            = "cool-accounts/terraform.tfstate"
-  }
-}
+# Comment out the contents of this file when bootstrapping this
+# account.
+# terraform {
+#   backend "s3" {
+#     encrypt        = true
+#     bucket         = "cisa-cool-terraform-state"
+#     dynamodb_table = "terraform-state-lock"
+#     profile        = "terraform-role"
+#     region         = "us-east-1"
+#     key            = "cool-accounts/terraform.tfstate"
+#   }
+# }
