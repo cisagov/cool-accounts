fc4cdead46305f3a7013244765d5f458e09ac296
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-31 14:35:44 -0500

    Put the backend config back in
    Both the terraform and users accounts have been deployed via SSO
    programmatic creds, so we can use the backend.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
 # Comment out the contents of this file when bootstrapping this
 # account.
-# terraform {
-#   backend "s3" {
-#     encrypt        = true
-#     bucket         = "cisa-cool-terraform-state"
-#     dynamodb_table = "terraform-state-lock"
-#     profile        = "cool-terraform-backend"
-#     region         = "us-east-1"
-#     key            = "cool-accounts/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/terraform.tfstate"
+  }
+}
