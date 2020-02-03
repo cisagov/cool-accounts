e413e7a6c3817cf7cf658a2d20966d0e89f2eafb
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-03 14:59:34 -0500

    Correct alphabetization of variables

--- users/variables.tf
+++ users/variables.tf
@@ -23,11 +23,6 @@ variable "this_account_id" {
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
-variable "aws_region" {
-  description = "The AWS region where the non-global resources for this account are to be provisioned (e.g. \"us-east-1\")."
-  default     = "us-east-1"
-}
-
 variable "assume_access_terraform_backend_policy_description" {
   description = "The description to associate with the IAM policy that allows assumption of the role with access to the Terraform backend."
   default     = "Allow assumption of the AccessTerraformBackend role in the Terraform account."
@@ -58,6 +53,11 @@ variable "assume_tf_provisionaccount_policy_name" {
   default     = "Terraform-AssumeProvisionAccount"
 }
 
+variable "aws_region" {
+  description = "The AWS region where the non-global resources for this account are to be provisioned (e.g. \"us-east-1\")."
+  default     = "us-east-1"
+}
+
 variable "iam_admin_role_description" {
   description = "The description to associate with the IAM role that allows full IAM administrator access in this account."
   default     = "Allows full IAM administrator access in this account."
@@ -73,6 +73,12 @@ variable "iam_admins_group" {
   default     = "iam_admins"
 }
 
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all AWS resources provisioned."
+  default     = {}
+}
+
 variable "terraform_account_provisioners_group_name" {
   description = "The name of the group to be created for users allowed to provision the Terraform account."
   default     = "terraform_account_provisioners"
@@ -81,10 +87,4 @@ variable "terraform_account_provisioners_group_name" {
 variable "terraform_backend_users_group" {
   description = "The name of the group to be created for users allowed to access the Terraform backend."
   default     = "terraform_backend_users"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to all AWS resources provisioned."
-  default     = {}
 }
