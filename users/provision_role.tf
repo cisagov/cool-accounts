393b37f0ef5b23baf2799e6cfa6876c7302bfd92
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-03 16:56:22 -0500

    Rename role file with "provision" name, and update contents accordingly

--- users/provision_role.tf
+++ users/provision_role.tf
@@ -3,7 +3,7 @@
 # AWS resources in the users account.
 # ------------------------------------------------------------------------------
 
-data "aws_iam_policy_document" "iam_admin_role_assume_role_doc" {
+data "aws_iam_policy_document" "provision_account_role_assume_role_doc" {
   statement {
     actions = [
       "sts:AssumeRole",
@@ -18,15 +18,15 @@ data "aws_iam_policy_document" "iam_admin_role_assume_role_doc" {
   }
 }
 
-resource "aws_iam_role" "iam_admin_role" {
-  assume_role_policy = data.aws_iam_policy_document.iam_admin_role_assume_role_doc.json
-  description        = var.iam_admin_role_description
-  name               = var.iam_admin_role_name
+resource "aws_iam_role" "provision_account_role" {
+  assume_role_policy = data.aws_iam_policy_document.provision_account_role_assume_role_doc.json
+  description        = var.provision_account_role_description
+  name               = var.provision_account_role_name
   tags               = var.tags
 }
 
 # Attach IAMFullAccess policy to the role
 resource "aws_iam_role_policy_attachment" "iamfullaccess_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-  role       = aws_iam_role.iam_admin_role.name
+  role       = aws_iam_role.provision_account_role.name
 }
