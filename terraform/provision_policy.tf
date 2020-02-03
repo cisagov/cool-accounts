c5033eb5050ef6ffec78bd3d179be4681a693519
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-03 11:30:03 -0500

    Change variable references in accordance with new names
    Also change resource names in accordance with new variable names.

--- terraform/provision_policy.tf
+++ terraform/provision_policy.tf
@@ -1,15 +1,12 @@
 # ------------------------------------------------------------------------------
-# Create the IAM policy that allows sufficient permissions to create
-# all AWS resources in the terraform account.
+# Create the IAM policy that allows sufficient permissions to
+# provision all AWS resources in the terraform account.
 # ------------------------------------------------------------------------------
 
-# IAM policy document that allows sufficient permissions to create all
-# non-IAM AWS resources in the terraform account.
-#
 # We will also attach the IAMFullAccess policy to the role that uses
-# this policy, so it will have all necessary permissions to create all
-# AWS resources in the terraform account.
-data "aws_iam_policy_document" "create_doc" {
+# this policy, so it will have all necessary permissions to provision
+# all AWS resources in the terraform account.
+data "aws_iam_policy_document" "provision_account_doc" {
   # Permissions necessary to manipulate the state bucket
   statement {
     actions = [
@@ -20,7 +17,7 @@ data "aws_iam_policy_document" "create_doc" {
     ]
   }
 
-  # Permissions necessary to create and manipulate the state lock table
+  # Permissions necessary to manipulate the state lock table
   statement {
     actions = [
       "dynamodb:*",
@@ -32,8 +29,8 @@ data "aws_iam_policy_document" "create_doc" {
 }
 
 # The IAM policy
-resource "aws_iam_policy" "create_policy" {
-  description = var.create_role_description
-  name        = var.create_role_name
-  policy      = data.aws_iam_policy_document.create_doc.json
+resource "aws_iam_policy" "provision_account_policy" {
+  description = var.provision_account_role_description
+  name        = var.provision_account_role_name
+  policy      = data.aws_iam_policy_document.provision_account_doc.json
 }
--- terraform/provision_role.tf
+++ terraform/provision_role.tf
@@ -1,22 +1,22 @@
 # ------------------------------------------------------------------------------
-# Create the IAM role that allows sufficient permissions to create all
-# AWS resources in the terraform account.
+# Create the IAM role that allows sufficient permissions to provision
+# all AWS resources in the terraform account.
 # ------------------------------------------------------------------------------
 
-resource "aws_iam_role" "create_role" {
+resource "aws_iam_role" "provision_account_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
-  description        = var.create_role_description
-  name               = var.create_role_name
+  description        = var.provision_account_role_description
+  name               = var.provision_account_role_name
   tags               = var.tags
 }
 
 # Attach the desired IAM policies to the role
-resource "aws_iam_role_policy_attachment" "create_policy_attachment" {
-  policy_arn = aws_iam_policy.create_policy.arn
-  role       = aws_iam_role.create_role.name
+resource "aws_iam_role_policy_attachment" "provision_account_policy_attachment" {
+  policy_arn = aws_iam_policy.provision_account_policy.arn
+  role       = aws_iam_role.provision_account_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "iamfullaccess_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-  role       = aws_iam_role.create_role.name
+  role       = aws_iam_role.provision_account_role.name
 }
