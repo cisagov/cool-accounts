0283a3d2dfacbb422f40b0e86b0a0f3620d129d5
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-03 11:27:09 -0500

    Update variable references to match new names
    Also change resource names in accordance with new variable names.

--- terraform/backend_role.tf
+++ terraform/backend_role.tf
@@ -3,15 +3,15 @@
 # and DynamoDB table to use them for a Terraform backend.
 # ------------------------------------------------------------------------------
 
-resource "aws_iam_role" "backend_role" {
+resource "aws_iam_role" "access_terraform_backend_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
-  description        = var.backend_role_description
-  name               = var.backend_role_name
+  description        = var.access_terraform_backend_role_description
+  name               = var.access_terraform_backend_role_name
   tags               = var.tags
 }
 
 # Attach the IAM policy to the role
-resource "aws_iam_role_policy_attachment" "backend_access_policy_attachment" {
-  policy_arn = aws_iam_policy.backend_access_policy.arn
-  role       = aws_iam_role.backend_role.name
+resource "aws_iam_role_policy_attachment" "access_terraform_backend_policy_attachment" {
+  policy_arn = aws_iam_policy.access_terraform_backend_access_policy.arn
+  role       = aws_iam_role.access_terraform_backend_role.name
 }
