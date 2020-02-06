3ca70a153d9cd7495dfd8f6529b3aa9dd65764e6
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-06 14:08:34 -0500

    Ensure that there is no possibility of public access to buckets
    Or the objects contained therein

--- dns/certificate_bucket.tf
+++ dns/certificate_bucket.tf
@@ -13,3 +13,14 @@ resource "aws_s3_bucket" "certificate_bucket" {
   }
   tags = var.tags
 }
+
+# This blocks ANY public access to the bucket or the objects it
+# contains, even if misconfigured to allow public access.
+resource "aws_s3_bucket_public_access_block" "certificate_bucket" {
+  bucket = aws_s3_bucket.certificate_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- terraform/state_bucket.tf
+++ terraform/state_bucket.tf
@@ -16,3 +16,14 @@ resource "aws_s3_bucket" "state_bucket" {
     enabled = true
   }
 }
+
+# This blocks ANY public access to the bucket or the objects it
+# contains, even if misconfigured to allow public access.
+resource "aws_s3_bucket_public_access_block" "state_bucket" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
