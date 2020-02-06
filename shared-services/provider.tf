e67b1c4b3111175a8296f539269a78194ae0cb70
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-06 11:36:45 -0500

    Correct incorrect reference to the terraform account

--- shared-services/provider.tf
+++ shared-services/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
     role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.provisionaccount_role_name}"
   }
   # Use this profile, defined using programmatic credentials for
-  # AWSAdministratorAccess as obtained for the COOL terraform account
-  # from the AWS SSO page, to bootstrap the account.
+  # AWSAdministratorAccess as obtained for the COOL Shared Services
+  # account from the AWS SSO page, to bootstrap the account.
   # profile = "cool-sharedservices-account-admin"
 }
