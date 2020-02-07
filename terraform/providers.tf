c2179fcde4b7f3f19496a953a1cbaac3c96edace
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-07 15:32:24 -0500

    Add some clarifying comments
    The Terraform account now must be bootstrapped using SSO programmatic
    creds for both the users and terraform accounts.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,13 +2,13 @@
 # inside the Terraform account
 provider "aws" {
   region = var.aws_region
-  # Use this role once the account has been bootstrapped.
+  # Use this role once the terraform account has been bootstrapped.
   assume_role {
     role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.provisionaccount_role_name}"
   }
   # Use this profile, defined using programmatic credentials for
   # AWSAdministratorAccess as obtained for the COOL Terraform account
-  # from the AWS SSO page, to bootstrap the account.
+  # from the AWS SSO page, to bootstrap the Terraform account.
   # profile = "cool-terraform-account-admin"
 }
 
@@ -17,7 +17,12 @@ provider "aws" {
 provider "aws" {
   alias  = "users"
   region = var.aws_region
+  # Use this role once the terraform account has been bootstrapped.
   assume_role {
     role_arn = "arn:aws:iam::${var.users_account_id}:role/${var.provisionaccount_role_name}"
   }
+  # Use this profile, defined using programmatic credentials for
+  # AWSAdministratorAccess as obtained for the COOL Terraform account
+  # from the AWS SSO page, to bootstrap the Terraform account.
+  # profile = "cool-users-account-admin"
 }
