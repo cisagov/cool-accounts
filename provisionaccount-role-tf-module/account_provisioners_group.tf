bf5dba599c10e33252e93094c088854fb527eb30
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-07 15:22:49 -0500

    Fix typo

--- provisionaccount-role-tf-module/account_provisioners_group.tf
+++ provisionaccount-role-tf-module/account_provisioners_group.tf
@@ -5,7 +5,7 @@ resource "aws_iam_group" "account_provisioners" {
   name = var.account_provisioners_group_name
 }
 
-# Put our admin IAM users in the account provisioners groups for the
+# Put our admin IAM users in the account provisioners group for the
 # new account.
 resource "aws_iam_group_membership" "account_provisioners" {
   provider = aws.users
