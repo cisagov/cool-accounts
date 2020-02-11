648c23348179e5c3ddcf326dab75455fc8642367
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-10 21:54:11 -0500

    Switch to using profiles instead of role ARNs in providers

--- users/provider.tf
+++ users/provider.tf
@@ -1,9 +1,7 @@
 provider "aws" {
   region = var.aws_region
-  # Use this role once the account has been bootstrapped.
-  assume_role {
-    role_arn = "arn:aws:iam::${var.this_account_id}:role/${var.provisionaccount_role_name}"
-  }
+  # Use this profile once the account has been bootstrapped.
+  profile = "cool-users-provisionaccount"
   # Use the profile below, defined using programmatic credentials for
   # AWSAdministratorAccess as obtained for the COOL "users" account
   # from the AWS SSO page, to bootstrap the account.
