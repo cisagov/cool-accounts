978ca28362407f7f9d3d81243cde46b91b91d90d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-13 13:16:48 -0500

    Change the users subdir to use the provisionaccount-role TF module
    Previously this role was creating the ProvisionAccount role using its
    own code.  Gotta keep it DRY.

--- users/outputs.tf
+++ users/outputs.tf
@@ -14,6 +14,6 @@ output "gods_group_arn" {
 }
 
 output "provisionaccount_role_arn" {
-  value       = aws_iam_role.provisionaccount_role.arn
+  value       = module.provisionaccount.provisionaccount_role_arn
   description = "The ARN of the IAM role that allows sufficient permissions to provision all AWS resources in this account."
 }
