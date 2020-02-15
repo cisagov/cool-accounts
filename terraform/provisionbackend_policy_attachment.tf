f431676c09976144caadf7578ffad031e9f2437c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-15 13:16:49 -0500

    Give the policy for provisioning the TF backend a better name and description

--- terraform/provisionbackend_policy_attachment.tf
+++ terraform/provisionbackend_policy_attachment.tf
@@ -3,7 +3,7 @@
 # the S3 bucket and DynamoDB table resources in the terraform account.
 # ------------------------------------------------------------------------------
 
-resource "aws_iam_role_policy_attachment" "provisionaccount_policy_attachment" {
-  policy_arn = aws_iam_policy.provisionaccount_policy.arn
+resource "aws_iam_role_policy_attachment" "provisionbackend_policy_attachment" {
+  policy_arn = aws_iam_policy.provisionbackend_policy.arn
   role       = module.provisionaccount.provisionaccount_role_name
 }
