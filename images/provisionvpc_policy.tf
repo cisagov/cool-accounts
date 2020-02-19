154777a027f84d41b35560f03bdb971099e179ab
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-19 16:23:15 -0500

    Define policy to allow all EC2 actions needed to provision VPCs

---
+++ images/provisionvpc_policy.tf
@@ -0,0 +1,67 @@
+# ------------------------------------------------------------------------------
+# Create the IAM policy that allows all of the EC2 actions necessary to
+# provision VPCs in the Images account
+# ------------------------------------------------------------------------------
+
+data "aws_iam_policy_document" "provisionvpcs_doc" {
+  statement {
+    actions = [
+      "ec2:AssociateRouteTable",
+      "ec2:AssociateVpcCidrBlock",
+      "ec2:AttachInternetGateway",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CreateInternetGateway",
+      "ec2:CreateRoute",
+      "ec2:CreateRouteTable",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateSubnet",
+      "ec2:CreateTags",
+      "ec2:CreateVpc",
+      "ec2:DeleteInternetGateway",
+      "ec2:DeleteRoute",
+      "ec2:DeleteRouteTable",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteSubnet",
+      "ec2:DeleteTags",
+      "ec2:DeleteVpc",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeInternetGateways",
+      "ec2:DescribeNatGateways",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroupReferences",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeTags",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeVpcClassicLink",
+      "ec2:DescribeVpcClassicLinkDnsSupport",
+      "ec2:DescribeVpcEndpointConnectionNotifications",
+      "ec2:DescribeVpcEndpointConnections",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeVpcEndpointServiceConfigurations",
+      "ec2:DescribeVpcEndpointServicePermissions",
+      "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribeVpcPeeringConnections",
+      "ec2:DescribeVpcs",
+      "ec2:DetachInternetGateway",
+      "ec2:DisassociateVpcCidrBlock",
+      "ec2:ModifySubnetAttribute",
+      "ec2:ModifyVpcAttribute",
+      "ec2:ReplaceRoute",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "provisionvpcs_policy" {
+  description = var.provisionvpcs_policy_description
+  name        = var.provisionvpcs_policy_name
+  policy      = data.aws_iam_policy_document.provisionvpcs_doc.json
+}
