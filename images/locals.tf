9510982aefb04c21d5c37be07ccf39c89d8a32b7
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-24 15:52:52 -0400

    Determine type of calling Images account (e.g. "Production" or "Staging").

--- images/locals.tf
+++ images/locals.tf
@@ -4,4 +4,19 @@ locals {
     "tcp",
     "udp",
   ]
+
+  # Get current Images account ID from Images provider
+  this_account_id = data.aws_caller_identity.images.account_id
+
+  # Look up current Images account name from AWS organizations provider
+  this_account_name = [
+    for account in data.aws_organizations_organization.cool.accounts :
+    account.name
+    if account.id == local.this_account_id
+  ][0]
+
+  # Determine Images account type based on AWS account name
+  # Account name format:  "ACCOUNT_NAME (ACCOUNT_TYPE)"
+  #         For example:  "Images (Production)"
+  this_account_type = length(regexall("\\((.*?)\\)", local.this_account_name)) == 1 ? regex("\\((.*?)\\)", local.this_account_name)[0] : "Unknown"
 }
