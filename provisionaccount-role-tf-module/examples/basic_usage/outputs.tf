3868b8561be4cfb1634f1d78849f569a26116276
Dave Redmin <david.redmin@beta.dhs.gov>
2020-11-10 13:30:30 -0500

    Remove local provisionaccount-role-tf-module

--- provisionaccount-role-tf-module/examples/basic_usage/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "provisionaccount_role_arn" {
-  value       = module.provisionaccount.provisionaccount_role_arn
-  description = "The ARN of the IAM role that allows sufficient permissions to provision all AWS resources in the Pettifogger0 account."
-}
--- provisionaccount-role-tf-module/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "provisionaccount_role" {
-  value       = aws_iam_role.provisionaccount_role
-  description = "The IAM role that allows sufficient permissions to provision all AWS resources in the new account."
-}
