a940fdea53f195037c4c627703efe12c858c745b
Dave Redmin <david.redmin@beta.dhs.gov>
2020-11-17 11:02:43 -0500

    Add variables needed for PCA Terraform backend role and policy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,6 +20,18 @@ variable "users_account_id" {
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
+variable "access_pca_terraform_backend_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient access to the PCA-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+  default     = "Allows sufficient access to the PCA-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+}
+
+variable "access_pca_terraform_backend_role_name" {
+  type        = string
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient access to the PCA-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+  default     = "AccessPCATerraformBackend"
+}
+
 variable "access_terraform_backend_role_description" {
   type        = string
   description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient access to the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
@@ -36,6 +48,12 @@ variable "aws_region" {
   type        = string
   description = "The AWS region where the non-global resources for this account are to be provisioned (e.g. \"us-east-1\")."
   default     = "us-east-1"
+}
+
+variable "pca_terraform_projects" {
+  type        = list(string)
+  description = "The list of project names that contain PCA-related Terraform code (e.g. [\"my-pca-project\"])."
+  default     = []
 }
 
 variable "provisionaccount_role_description" {
