98b086c71dc7956ab289d2a4fed2281b0fff7625
David Redmin <david.redmin@trio.dhs.gov>
2021-02-03 15:46:00 -0500

    Add variables needed for Domain Manager Terraform backend role and policy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,6 +20,18 @@ variable "users_account_id" {
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
+variable "access_domainmanager_terraform_backend_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient access to the Domain Manager-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+  default     = "Allows sufficient access to the Domain Manager-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+}
+
+variable "access_domainmanager_terraform_backend_role_name" {
+  type        = string
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient access to the Domain Manager-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
+  default     = "AccessDomainManagerTerraformBackend"
+}
+
 variable "access_pca_terraform_backend_role_description" {
   type        = string
   description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient access to the PCA-related items in the Terraform S3 bucket and DynamoDB table to use those resources as a Terraform backend."
@@ -48,6 +60,12 @@ variable "aws_region" {
   type        = string
   description = "The AWS region where the non-global resources for this account are to be provisioned (e.g. \"us-east-1\")."
   default     = "us-east-1"
+}
+
+variable "domainmanager_terraform_projects" {
+  type        = list(string)
+  description = "The list of project names that contain Domain Manager-related Terraform code (e.g. [\"my-domain-manager-project\"])."
+  default     = []
 }
 
 variable "pca_terraform_projects" {
