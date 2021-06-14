8611a2e4b6f76f06fab6fde76c5086ae163630e2
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-06-14 15:48:13 -0400

    Pass default tags via providers

--- shared-services/ssm_session_role.tf
+++ shared-services/ssm_session_role.tf
@@ -7,7 +7,6 @@ resource "aws_iam_role" "ssmsession_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_doc.json
   description        = var.ssmsession_role_description
   name               = var.ssmsession_role_name
-  tags               = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "ssmsession_policy_attachment" {
