b152e432a6e27e95bdc86149cc87056af0fb4f5c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-08-17 13:48:02 -0400

    Attach org read-only policy to provisionaccount role
    This is necessary since we need to get a list of assessment accounts
    from the organization in order to create the org read-only role in the
    first place.  We cannot use the org read-only role for this purpose
    because it would lead to a cycle in the Terraform state.

--- master/provisionaccount.tf
+++ master/provisionaccount.tf
@@ -5,3 +5,11 @@ module "provisionaccount" {
   provisionaccount_role_name        = var.provisionaccount_role_name
   users_account_id                  = var.users_account_id
 }
+
+# Attach a policy allowing this role to read organization information.
+# This is necessary since we can't use the role created in this repo
+# for this purpose since we need to do this to create that role.
+resource "aws_iam_role_policy_attachment" "read_organization" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSOrganizationsReadOnlyAccess"
+  role       = module.provisionaccount.provisionaccount_role.name
+}
