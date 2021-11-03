f2d4e1d5701b3705fe8d7fa7fa6c7edd13ab63d8
David Redmin <david.redmin@trio.dhs.gov>
2021-11-03 11:22:12 -0400

    Add additional permissions needed by Control Tower
    Also:
    * Remove AWSControlTowerServiceRolePolicy
    * Rename/reorder policy attachments
    * Improve comments

--- master/controltoweradmin_role.tf
+++ master/controltoweradmin_role.tf
@@ -3,9 +3,47 @@
 # AWS accounts via Control Tower.
 # ------------------------------------------------------------------------------
 
-data "aws_iam_policy_document" "describeavailabilityzones_policy" {
+# These are additional permissions needed besides those found in the managed
+# AWS policies attached below.  These permissions are documented in the
+# Control Tower User Guide in the "Automated Account Provisioning With
+# IAM Roles" section:
+# https://docs.aws.amazon.com/controltower/latest/userguide/controltower-ug.pdf
+data "aws_iam_policy_document" "additionalpermissions_policy" {
   statement {
-    actions   = ["ec2:DescribeAvailabilityZones"]
+    actions = [
+      "controltower:CreateManagedAccount",
+      "controltower:DeregisterManagedAccount",
+      "controltower:DescribeManagedAccount",
+      "ec2:DescribeAvailabilityZones",
+      "sso:AssociateProfile",
+      "sso:CreateApplicationInstance",
+      "sso:CreateProfile",
+      "sso:CreateTrust",
+      "sso:DescribeRegisteredRegions",
+      "sso:GetApplicationInstance",
+      "sso:GetPeregrineStatus",
+      "sso:GetPermissionSet",
+      "sso:GetProfile",
+      "sso:GetSSOStatus",
+      "sso:GetTrust",
+      "sso:ListDirectoryAssociations",
+      "sso:ListPermissionSets",
+      "sso:ListProfileAssociations",
+      "sso:ProvisionApplicationInstanceForAWSAccount",
+      "sso:ProvisionApplicationProfileForAWSAccountInstance",
+      "sso:ProvisionSAMLProvider",
+      "sso:UpdateProfile",
+      "sso:UpdateTrust",
+      "sso-directory:AddMemberToGroup",
+      "sso-directory:CreateUser",
+      "sso-directory:DescribeDirectory",
+      "sso-directory:DescribeGroups",
+      "sso-directory:GetUserPoolInfo",
+      "sso-directory:ListMembersInGroup",
+      "sso-directory:SearchGroups",
+      "sso-directory:SearchGroupsWithGroupName",
+      "sso-directory:SearchUsers",
+    ]
     resources = ["*"]
   }
 }
@@ -13,11 +51,11 @@ data "aws_iam_policy_document" "describeavailabilityzones_policy" {
 resource "aws_iam_role" "controltoweradmin_role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_users_only_doc.json
   description        = var.controltoweradmin_role_description
-  # This role requires the EC2 permission below in addition to the standard
+  # This role requires the permissions below in addition to the standard
   # AWS policies (attached further below) to function as intended.
   inline_policy {
-    name   = "DescribeAvailabilityZonesPolicy"
-    policy = data.aws_iam_policy_document.describeavailabilityzones_policy.json
+    name   = "AdditionalControlTowerPermissionsPolicy"
+    policy = data.aws_iam_policy_document.additionalpermissions_policy.json
   }
   # We use the largest possible maximum session duration value of 12 hours
   # since this role can be used to provision many AWS accounts consecutively
@@ -29,17 +67,12 @@ resource "aws_iam_role" "controltoweradmin_role" {
 }
 
 # Attach necessary IAM policies to the role
-resource "aws_iam_role_policy_attachment" "awscontroltowerservicerole_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSControlTowerServiceRolePolicy"
-  role       = aws_iam_role.controltoweradmin_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "awsservicecatalogadmin_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSServiceCatalogAdminFullAccess"
-  role       = aws_iam_role.controltoweradmin_role.name
-}
-
 resource "aws_iam_role_policy_attachment" "organizationsfullaccess_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AWSOrganizationsFullAccess"
+  role       = aws_iam_role.controltoweradmin_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "servicecatalogadminfullaccess_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSServiceCatalogAdminFullAccess"
   role       = aws_iam_role.controltoweradmin_role.name
 }
