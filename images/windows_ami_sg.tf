ca2d2f31ddc3a3a353d013c0b9e8b7527af45296
Nick M <50747025+mcdonnnj@users.noreply.github.com>
2021-11-08 13:32:20 -0500

    DRY out some Security Group rules
    Co-authored-by: Shane Frasier <jeremy.frasier@trio.dhs.gov>

--- images/windows_ami_sg.tf
+++ images/windows_ami_sg.tf
@@ -11,21 +11,13 @@ resource "aws_security_group" "windows_ami" {
   }
 }
 
-# Allow ingress to instances via RDP (TCP)
-resource "aws_security_group_rule" "windows_ami_ingress_via_rdp_over_tcp" {
-  security_group_id = aws_security_group.windows_ami.id
-  type              = "ingress"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 3389
-  to_port           = 3389
-}
+# Allow ingress to instances via RDP
+resource "aws_security_group_rule" "windows_ami_ingress_via_rdp" {
+  for_each = toset(["tcp", "udp"])
 
-# Allow ingress to instances via RDP (UDP)
-resource "aws_security_group_rule" "windows_ami_ingress_via_rdp_over_udp" {
   security_group_id = aws_security_group.windows_ami.id
   type              = "ingress"
-  protocol          = "udp"
+  protocol          = each.value
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 3389
   to_port           = 3389
