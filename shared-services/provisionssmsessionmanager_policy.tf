f956a051d017a420d80d972965fa0d212e907a6a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-04-19 15:49:05 -0400

    Add more permissions to policy for provisioning the SSM Session Manager
    These permissions were found to be missing when working on cisagov/cool-assessment-terraform#186.

--- shared-services/provisionssmsessionmanager_policy.tf
+++ shared-services/provisionssmsessionmanager_policy.tf
@@ -5,8 +5,10 @@
 # ------------------------------------------------------------------------------
 
 data "aws_iam_policy_document" "provisionssmsessionmanager_policy_doc" {
+  # SSM document permissions
   statement {
     actions = [
+      "ssm:AddTagsToResource",
       "ssm:CreateDocument",
       "ssm:DeleteDocument",
       "ssm:DescribeDocument*",
@@ -16,6 +18,30 @@ data "aws_iam_policy_document" "provisionssmsessionmanager_policy_doc" {
 
     resources = [
       "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.sharedservices.account_id}:document/SSM-SessionManagerRunShell",
+    ]
+  }
+
+  # CloudWatch log group permissions
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:DescribeLogGroups",
+      "logs:ListTagsLogGroup",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+  statement {
+    actions = [
+      "logs:DeleteLogGroup",
+      "logs:PutRetentionPolicy",
+      "logs:TagLogGroup",
+    ]
+
+    resources = [
+      "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.sharedservices.account_id}:log-group:${module.session_manager.ssm_session_log_group.name}:*",
     ]
   }
 }
