119c28b3016de5422a84d27e00b9da6a7ae01e88
David Redmin <david.redmin@trio.dhs.gov>
2022-07-06 15:07:44 -0400

    Add optional variables related to egress IP publishing
    For more info, see cisagov/publish-egress-ip-terraform.

--- dns/variables.tf
+++ dns/variables.tf
@@ -22,6 +22,18 @@ variable "provisionaccount_role_name" {
   default     = "ProvisionAccount"
 }
 
+variable "provisionpublishegressip_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to provision all resources related to the publish-egress-ip Lambda in the DNS account."
+  default     = "Allows sufficient permissions to provision all resources related to the publish-egress-ip Lambda in the DNS account."
+}
+
+variable "provisionpublishegressip_role_name" {
+  type        = string
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to provision all resources related to the publish-egress-ip Lambda in the DNS account."
+  default     = "ProvisionPublishEgressIP"
+}
+
 variable "provisionroute53_role_description" {
   type        = string
   description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to provision Route 53 in the DNS account."
@@ -32,6 +44,18 @@ variable "provisionroute53_role_name" {
   type        = string
   description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to provision Route 53 in the DNS account."
   default     = "ProvisionRoute53"
+}
+
+variable "publishegressip_lambda_name" {
+  type        = string
+  description = "The name of the Lambda function used in cisagov/publish-egress-ip-terraform.  This name is used to specify resource constraints in the role/policy specified in var.provisionpublishegressip_role_name."
+  default     = "publish-egress-ip"
+}
+
+variable "publishegressip_role_name" {
+  type        = string
+  description = "The name of the IAM role (meant to be used in cisagov/publish-egress-ip-terraform) that is allowed to be created by the role/policy specified in var.provisionpublishegressip_role_name."
+  default     = "PublishEgressIPLambda"
 }
 
 variable "tags" {
