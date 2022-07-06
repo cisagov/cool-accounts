803159b670cda7788e5498d0cc468eb5f55976ae
David Redmin <david.redmin@trio.dhs.gov>
2022-07-06 15:03:49 -0400

    Add local variable for DNS account ID

--- dns/locals.tf
+++ dns/locals.tf
@@ -5,7 +5,16 @@ data "aws_organizations_organization" "cool" {
   provider = aws.organizationsreadonly
 }
 
+# ------------------------------------------------------------------------------
+# We can get the account ID of the DNS account from the default provider's
+# caller identity.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "dns" {
+}
+
 locals {
+  dns_account_id = data.aws_caller_identity.dns.account_id
+
   # Find the Users account
   users_account_id = [
     for account in data.aws_organizations_organization.cool.accounts :
