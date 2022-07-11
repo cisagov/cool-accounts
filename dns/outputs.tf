d2de7cdf53f981720d62dcd11299d997c9a7d785
David Redmin <david.redmin@trio.dhs.gov>
2022-07-11 14:46:18 -0400

    Alphabetize all keys in outputs file

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,19 +1,19 @@
 output "cw_alarm_sns_topic" {
-  value       = module.cw_alarm_sns.sns_topic
   description = "The SNS topic to which a message is sent when a CloudWatch alarm is triggered."
+  value       = module.cw_alarm_sns.sns_topic
 }
 
 output "provisionaccount_role" {
-  value       = module.provisionaccount.provisionaccount_role
   description = "The IAM role that allows sufficient permissions to provision all AWS resources in the DNS account."
+  value       = module.provisionaccount.provisionaccount_role
 }
 
 output "provisionpublishegressip_role" {
-  value       = aws_iam_role.provisionpublishegressip_role
   description = "The IAM role that allows sufficient permissions to provision all resources related to the publish-egress-ip Lambda in the DNS account."
+  value       = aws_iam_role.provisionpublishegressip_role
 }
 
 output "primary_delegation_set" {
-  value       = aws_route53_delegation_set.primary
   description = "The primary reusable delegation set that contains the authoritative name servers for all public DNS zones."
+  value       = aws_route53_delegation_set.primary
 }
