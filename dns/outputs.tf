69365712af3d7198735f4f6e3d916f20662dd485
David Redmin <david.redmin@trio.dhs.gov>
2022-07-11 15:00:11 -0400

    Alphabetize outputs by name

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -3,6 +3,11 @@ output "cw_alarm_sns_topic" {
   value       = module.cw_alarm_sns.sns_topic
 }
 
+output "primary_delegation_set" {
+  description = "The primary reusable delegation set that contains the authoritative name servers for all public DNS zones."
+  value       = aws_route53_delegation_set.primary
+}
+
 output "provisionaccount_role" {
   description = "The IAM role that allows sufficient permissions to provision all AWS resources in the DNS account."
   value       = module.provisionaccount.provisionaccount_role
@@ -11,9 +16,4 @@ output "provisionaccount_role" {
 output "provisionpublishegressip_role" {
   description = "The IAM role that allows sufficient permissions to provision all resources related to the publish-egress-ip Lambda in the DNS account."
   value       = aws_iam_role.provisionpublishegressip_role
-}
-
-output "primary_delegation_set" {
-  description = "The primary reusable delegation set that contains the authoritative name servers for all public DNS zones."
-  value       = aws_route53_delegation_set.primary
 }
