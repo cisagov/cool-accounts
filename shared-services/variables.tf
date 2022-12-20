9bf105ec142cf193962fd9c9ae084b28c702a7b0
David Redmin <david.redmin@trio.dhs.gov>
2022-12-20 12:10:28 -0500

    Add variables related to policy/role for writing to assessment findings data bucket

--- shared-services/variables.tf
+++ shared-services/variables.tf
@@ -1,8 +1,38 @@
 # ------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+#
+# You must provide a value for each of these parameters.
+# ------------------------------------------------------------------------------
+
+# This bucket is created by cisagov/findings-data-import-terraform.
+variable "assessment_findings_bucket_arn" {
+  type        = string
+  description = "The ARN of the assessment findings S3 bucket."
+}
+
+# ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
+
+variable "assessment_findings_bucket_object_name_pattern" {
+  type        = string
+  description = "The name pattern specifying which objects are allowed to be written to the assessment findings data S3 bucket."
+  default     = "*-data.json"
+}
+
+variable "assessment_findings_bucket_write_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role that allows write access to the assessment findings S3 bucket."
+  default     = "Allows write permissions to the assessment findings S3 bucket."
+}
+
+variable "assessment_findings_bucket_write_role_name" {
+  type        = string
+  description = "The name to assign the IAM role that allows write access to the assessment findings S3 bucket."
+  default     = "AssessmentFindingsBucketWrite"
+}
 
 variable "aws_region" {
   type        = string
