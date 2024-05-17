cc210f0a964b08029d4bbd3821409e2d7a17bd89
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-05-17 14:57:25 -0400

    Add two additional permissions that are now necessary

--- dns/provisionpublishegressip_policy.tf
+++ dns/provisionpublishegressip_policy.tf
@@ -163,6 +163,8 @@ data "aws_iam_policy_document" "provisionpublishegressip_doc" {
       "s3:ListBucket",
       "s3:ListBucketVersions",
       "s3:PutBucketAcl",
+      "s3:PutBucketOwnershipControls",
+      "s3:PutBucketPolicy",
       "s3:PutBucketPublicAccessBlock",
       "s3:PutBucketTagging",
       "s3:PutBucketVersioning",
