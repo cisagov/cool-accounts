d0b4df45b5f18681a7f012b82a186642bdf10ff7
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-09-06 13:52:40 -0400

    Remove errant character
    Co-authored-by: Dave Redmin <david.redmin@gwe.cisa.dhs.gov>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -140,7 +140,7 @@ variable "tags" {
 }
 
 variable "write_lambda_bucket_role_description" {
-  default     = "Allows sufficient permissions to write to the bucket that contains Lambda deployment packages] in the Terraform account."
+  default     = "Allows sufficient permissions to write to the bucket that contains Lambda deployment packages in the Terraform account."
   description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to write to the bucket that contains Lambda deployment packages in the Terraform account."
   type        = string
 }
