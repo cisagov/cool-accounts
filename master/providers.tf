fffa7a2d1e4fa168c302330797d086d58afd0b53
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-12-11 14:50:14 -0500

    Update bootstrap comments and instructions for master account

--- master/providers.tf
+++ master/providers.tf
@@ -19,6 +19,11 @@ provider "aws" {
   default_tags {
     tags = var.tags
   }
+  # Use this profile once the account has been bootstrapped.
   profile = "cool-master-organizationsreadonly"
-  region  = var.aws_region
+  # Use this profile, defined using programmatic credentials for
+  # AWSAdministratorAccess as obtained for the COOL Master account
+  # from the AWS SSO page, to bootstrap the account.
+  # profile = "cool-master-account-admin"
+  region = var.aws_region
 }
