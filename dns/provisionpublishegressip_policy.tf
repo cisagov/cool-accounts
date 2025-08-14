48050343097412060284186f923aa47dbbd0c2e2
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-08-14 09:45:09 -0400

    Add additional permissions needed for provisionpublishegressip policy
    These permissions were previously overlooked because they are only necessary after the "publish egress IP" Lambda has already been deployed.

--- dns/provisionpublishegressip_policy.tf
+++ dns/provisionpublishegressip_policy.tf
@@ -145,6 +145,7 @@ data "aws_iam_policy_document" "provisionpublishegressip_doc" {
       "logs:CreateLogGroup",
       "logs:DeleteLogGroup",
       "logs:DescribeLogGroups",
+      "logs:ListTagsForResource",
       "logs:ListTagsLogGroup",
       "logs:PutRetentionPolicy",
       "logs:TagResource",
@@ -152,10 +153,14 @@ data "aws_iam_policy_document" "provisionpublishegressip_doc" {
 
     resources = [
       format("arn:aws:logs:*:%s:log-group::log-stream:*", local.dns_account_id),
+      format("arn:aws:logs:*:%s:log-group:/aws/lambda/%s:log-stream:*",
+      local.dns_account_id, var.publishegressip_lambda_name),
+      format("arn:aws:logs:*:%s:log-group:/aws/lambda/%s", local.dns_account_id,
+      var.publishegressip_lambda_name),
       format("arn:aws:logs:*:%s:log-group:/aws/lambda/add_security_headers:log-stream:*",
       local.dns_account_id),
-      format("arn:aws:logs:*:%s:log-group:/aws/lambda/%s:log-stream:*",
-      local.dns_account_id, var.publishegressip_lambda_name)
+      format("arn:aws:logs:*:%s:log-group:/aws/lambda/add_security_headers",
+      local.dns_account_id),
     ]
   }
 
