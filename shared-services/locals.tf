35baaa67abed8252fcc19efeb950636d324ab527
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-04-28 15:45:26 -0400

    Update shared services Terraform to align with current account naming scheme
    The legacy account naming scheme is no longer in use.

--- shared-services/locals.tf
+++ shared-services/locals.tf
@@ -19,20 +19,8 @@ locals {
     x.name if x.id == data.aws_caller_identity.sharedservices.account_id
   ][0]
 
-  # Determine the dynamic assessment account ("env*") IDs that are the same
-  # type (production, staging, etc.) as the Shared Services account.
-  # Account name format:  "ACCOUNT_NAME (ACCOUNT_TYPE)"
-  #         For example:  "Shared Services (Production)"
-  # NOTE: Originally, Shared Services (and dynamic) account names followed the
-  # "ACCOUNT_NAME (ACCOUNT_TYPE)" format above, but our thinking has changed and
-  # in newer environments the accounts are simply called "Shared Services" and
-  # "env0" (for example).  However, until all legacy environments have been
-  # migrated to this new naming scheme, we must check the Shared Services
-  # account name via the regex below to determine whether we are using the
-  # legacy naming scheme or not.
-  sharedservices_account_name_type = length(regexall("\\(([^()]*)\\)", local.sharedservices_account_name)) == 1 ? "legacy" : "current"
-
-  assessment_account_name_regex = local.sharedservices_account_name_type == "legacy" ? format("^env[[:digit:]]+ \\(%s\\)$", trim(split("(", local.sharedservices_account_name)[1], ")")) : "^env[[:digit:]]+$"
+  # Regex to match dynamic assessment account names
+  assessment_account_name_regex = "^env[[:digit:]]+$"
 
   # Build a list of dynamic assessment account IDs whose account names match our
   # regex.
